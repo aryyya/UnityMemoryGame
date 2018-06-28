a0152e2424e98ce3d423dc7a760d72c1e3b48b03
aryyya <arya@live.ca>
2018-06-27 19:58:13 -0700

    Fix asset names, add card click script.

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject _cardBack;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("Memory card clicked.");
+    }
+}
