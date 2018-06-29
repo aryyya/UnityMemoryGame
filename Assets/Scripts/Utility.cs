cdead24063bd73bfcb971a0501717b9eaf2203f7
aryyya <arya@live.ca>
2018-06-28 17:11:12 -0700

    Add utility namespace.

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+public class Utility
+{
+    private static System.Random _random = new System.Random();
+
+    public static void Shuffle(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int randomIndex = _random.Next(0, list.Count - 1);
+            int currentElement = list[i];
+            list[i] = list[randomIndex];
+            list[randomIndex] = currentElement;
+        }
+    }
+
+    public static string ToString(List<int> list)
+    {
+        string output = "{ ";
+        for (int i = 0; i < list.Count; i++)
+        {
+            output += list[i].ToString();
+            if (i < list.Count - 1)
+            {
+                output += ", ";
+            }
+        }
+        return output + " }";
+    }
+}
